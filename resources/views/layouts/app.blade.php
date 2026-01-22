7d93924a6a8e51a9e424766f6b6058cb81bb5e90
pbhuwa <bhuwanpariyarr@gmail.com>
2026-01-22 18:00:54 +0545

    feat: Add toaster.js script for enhanced notifications

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,7 @@
         <script src="https://unpkg.com/@popperjs/core@2"></script>
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
         <script src="{{ asset('assets/plugins/js/jquery-3.7.1.js') }}"></script>
+        <script src="{{ asset('assets/plugins/js/toaster.js') }}"></script>
         <script src="{{ asset('assets/js/script.js') }}"></script>
 
         <script type="text/javascript">
